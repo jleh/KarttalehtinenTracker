7c137fe630a6f517473d2cf9ee49782a99064b56
jleh <jleh@karttalehtinen.fi>
2013-02-22 18:28:03 +0200

    Fixed GPX file time

--- service/gpx.php
+++ service/gpx.php
@@ -8,7 +8,7 @@
 
     echo "<trk><name>Ilveshiihto 2012</name><trkseg>";
 
-    $kysely = $yhteys->prepare("SELECT * FROM gps ORDER BY time ASC");
+    $kysely = $yhteys->prepare("SELECT * FROM gps WHERE event = 2 ORDER BY time ASC");
     $kysely->execute();
 
     $points = array();
@@ -19,8 +19,8 @@
                           "sat" => $rivi[satellites],
                           "stop" => $rivi[stop]);
          echo '<trkpt lat="'.$rivi[lat].'" lon="'.$rivi[long].'">';
-         echo '<ele>4.46</ele><time>2009-10-17T18:37:26Z</time></trkpt>';
+         echo '<ele>4.46</ele><time>' . date("c", $rivi[time]) . '</time></trkpt>';
     }
-
+    
     echo "</trkseg></trk></gpx>";
 ?>
